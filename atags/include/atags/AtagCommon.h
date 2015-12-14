5e59a0b15d390c2a86e9c16f7f3249527ab8621e
Humphrey Hu <humhu@cmu.edu>
2015-12-14 16:45:52 -0500

    Removed old atag msg code.

--- atags/include/atags/AtagCommon.h
+++ atags/include/atags/AtagCommon.h
@@ -5,26 +5,10 @@
 #include "apriltags/TagDetection.h"
 #include "argus_utils/PoseSE3.h"
 
-#include "argus_msgs/TagDetection.h"
-#include "argus_msgs/TagDetectionsStamped.h"
 #include "argus_msgs/FiducialDetection.h"
 
 namespace atags 
 {
-
-/*! \brief Convert to and from a tag detection message. */
-AprilTags::TagDetection MessageToDetection( const argus_msgs::TagDetection& msg );
-argus_msgs::TagDetection DetectionToMessage( const AprilTags::TagDetection& det,
-                                             const std::string& family,
-                                             bool undistorted, bool normalized );
-
-/*! \brief Convert to and from a tag-image detections message. */
-std::vector<AprilTags::TagDetection> MessageToDetections( const argus_msgs::TagDetectionsStamped& msg );
-
-argus_msgs::TagDetectionsStamped 
-DetectionsToMessage( const std::vector<AprilTags::TagDetection>& detections, 
-                     const std::string& family, const sensor_msgs::CameraInfo& info, 
-                     bool undistorted, bool normalized);
 
 /*! \brief Convert to FiducialDetection type. */
 argus_msgs::FiducialDetection TagToFiducial( const AprilTags::TagDetection& tag,
--- atags/src/AtagCommon.cpp
+++ atags/src/AtagCommon.cpp
@@ -6,50 +6,6 @@ using namespace argus_utils;
 
 namespace atags 
 {
-	
-AprilTags::TagDetection MessageToDetection( const argus_msgs::TagDetection& msg )
-{
-	AprilTags::TagDetection det;
-	det.good = true;
-	det.id = msg.id;
-	det.hammingDistance = msg.hammingDistance;
-	double xacc = 0, yacc = 0;
-	for( unsigned int i = 0; i < 4; i++ )
-	{
-		det.p[i].first = msg.corners[i].x;
-		det.p[i].second = msg.corners[i].y;
-		xacc += msg.corners[i].x;
-		yacc += msg.corners[i].y;
-	}
-	det.cxy.first = xacc/4.0;
-	det.cxy.second = yacc/4.0;
-	
-	// TODO Parameters we can't populate correctly?
-	det.obsCode = 0;
-	det.code = 0;
-	det.observedPerimeter = 0.0;
-	det.homography = Eigen::Matrix3d::Identity();
-	det.hxy = std::pair<float,float>( 0.0, 0.0 );
-	return det;
-}
-
-argus_msgs::TagDetection DetectionToMessage( const AprilTags::TagDetection& det,
-                                             const std::string& family,
-                                             bool undistorted, bool normalized )
-{
-	argus_msgs::TagDetection msg;
-	msg.family = family;
-	msg.id = det.id;
-	msg.hammingDistance = det.hammingDistance;
-	msg.undistorted = undistorted;
-	msg.normalized = normalized;
-	for( unsigned int i = 0; i < 4; i++ )
-	{
-		msg.corners[i].x = det.p[i].first;
-		msg.corners[i].y = det.p[i].second;
-	}
-	return msg;
-}
 
 argus_msgs::FiducialDetection TagToFiducial( const AprilTags::TagDetection& tag,
                                              const std::string& family )
@@ -67,34 +23,6 @@ argus_msgs::FiducialDetection TagToFiducial( const AprilTags::TagDetection& tag,
 		det.points.push_back( point );
 	}
 	return det;
-}
-
-std::vector<AprilTags::TagDetection> 
-MessageToDetections( const argus_msgs::TagDetectionsStamped& msg )
-{
-	std::vector<AprilTags::TagDetection> detections;
-	detections.reserve( msg.detections.size() );
-	for( unsigned int i = 0; i < msg.detections.size(); i++ )
-	{
-		detections.push_back( MessageToDetection( msg.detections[i] ) );
-	}
-	return detections;
-}
-
-argus_msgs::TagDetectionsStamped 
-DetectionsToMessage( const std::vector<AprilTags::TagDetection>& detections, 
-                     const std::string& family, const sensor_msgs::CameraInfo& info, 
-                     bool undistorted, bool normalized)
-{
-	argus_msgs::TagDetectionsStamped msg;
-	msg.detections.reserve( detections.size() );
-	for( unsigned int i = 0; i < detections.size(); i++ )
-	{
-		msg.detections.push_back( DetectionToMessage( detections[i], family,
-		                                              undistorted, normalized ) );
-	}
-	msg.info = info;
-	return msg;
 }
 
 argus_utils::PoseSE3 ComputeTagPose( const AprilTags::TagDetection& det, double tagSize,
