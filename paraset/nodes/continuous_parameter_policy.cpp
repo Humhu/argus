85af5b10d680ff3bdbffa5b7efe62613f457b3f2
Humphrey Hu <humhu@cmu.edu>
2016-09-06 01:21:33 -0400

    Various bugfixes for continuous parameter policy.

--- paraset/nodes/continuous_parameter_policy.cpp
+++ paraset/nodes/continuous_parameter_policy.cpp
@@ -4,6 +4,7 @@
 #include "argus_utils/utils/ParamUtils.h"
 
 using namespace argus;
+using namespace percepto;
 
 class ContinuousParameterPolicyNode
 {
@@ -31,7 +32,25 @@ private:
 
 	void TimerCallback( const ros::TimerEvent& event )
 	{
-		_manager.Execute( event.current_real );
+		try
+		{
+			ContinuousAction action = _manager.Execute( event.current_real );
+			
+			ContinuousPolicy::DistributionParameters dist = _manager.GetNormalizedDistribution( event.current_real );
+			std::vector<std::string> paramNames = _interface.GetParameterNames();
+			std::stringstream ss;
+			for( unsigned int i = 0; i < action.output.size(); i++ )
+			{
+				ss << paramNames[i] << ": " << action.output(i) << std::endl;
+			}
+
+			ROS_INFO_STREAM( "Distribution: " << std::endl << dist << std::endl <<
+			                 "Action: " << std::endl << ss.str() );
+		}
+		catch( std::out_of_range )
+		{
+			ROS_WARN_STREAM( "Could not execute policy at time: " << event.current_real );
+		}
 	}
 
 };
