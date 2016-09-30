9ac3cfbd3bc456d64bea3351b4349c5ab4160997
Humphrey Hu <humhu@cmu.edu>
2016-09-29 20:58:56 -0400

    Overhauled pose reference resolver.

--- fieldtrack/nodes/pose_reference_resolver.cpp
+++ fieldtrack/nodes/pose_reference_resolver.cpp
@@ -1,7 +1,12 @@
 #include <ros/ros.h>
 
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
 #include "argus_msgs/RelativePose.h"
+#include "argus_msgs/RelativePoseWithCovariance.h"
 #include "argus_utils/geometry/GeometryUtils.h"
+#include "argus_utils/utils/ParamUtils.h"
 #include "lookup/LookupInterface.h"
 #include "extrinsics_array/ExtrinsicsInfoManager.h"
 
@@ -12,99 +17,167 @@ class PoseReferenceResolver
 {
 public:
 
-	PoseReferenceResolver( const ros::NodeHandle& nh, const ros::NodeHandle& ph )
-	: nodeHandle( nh ), privHandle( ph ), lookupInterface(),
-	extrinsicsManager( lookupInterface )
+	PoseReferenceResolver( ros::NodeHandle& nh, ros::NodeHandle& ph )
+	: _lookupInterface(),
+	_extrinsicsManager( _lookupInterface )
 	{
-		if( !privHandle.getParam( "reference_name", referenceName ) )
-		{
-			ROS_ERROR_STREAM( "Please specify reference frame name." );
-			exit( -1 );
-		}
+		// if( !ph.getParam( "reference_name", _referenceName ) )
+		// {
+		// 	ROS_ERROR_STREAM( "Please specify reference frame name." );
+		// 	exit( -1 );
+		// }
 		
+		GetParamRequired( ph, "resolve_observer", _resolveObserver );
+		GetParamRequired( ph, "resolve_target", _resolveTarget );
+
 		std::string lookupNamespace;
-		privHandle.param<std::string>( "lookup_namespace", lookupNamespace, "/lookup" );
-		lookupInterface.SetLookupNamespace( lookupNamespace );
+		ph.param<std::string>( "lookup_namespace", lookupNamespace, "/lookup" );
+		_lookupInterface.SetLookupNamespace( lookupNamespace );
 		
-		poseSub = nodeHandle.subscribe( "relative_poses", 10, 
-		                                &PoseReferenceResolver::PoseCallback, 
-                                        this );
-		posePub = nodeHandle.advertise<argus_msgs::RelativePose>( "poses", 10 );
+		// TODO In/Out buffer sizes
+
+		std::string inputType;
+		unsigned int inBuffSize;
+		GetParamRequired( ph, "input_type", inputType );
+		GetParam( ph, "input_buff_size", inBuffSize, (unsigned int) 10 );
+		if( inputType == "relative_pose" )
+		{
+			_poseSub = nh.subscribe( "relative_pose",
+				                     inBuffSize, 
+			                         &PoseReferenceResolver::PoseCallback, 
+	                                 this );
+		}
+		else if( inputType == "relative_pose_with_cov" )
+		{
+			_poseSub = nh.subscribe( "relative_pose",
+				                     inBuffSize, 
+			                         &PoseReferenceResolver::PoseWithCovCallback, 
+	                                 this );
+		}
+		else
+		{
+			throw std::invalid_argument( "Unknown input type: " + inputType );
+		}
+
+		unsigned int outBuffSize;
+		GetParamRequired( ph, "output_type", _outputType );
+		GetParam( ph, "output_buff_size", outBuffSize, (unsigned int) 10 );
+		if( _outputType == "pose" )
+		{
+			_posePub = nh.advertise<geometry_msgs::Pose>( "resolved_pose", 
+				                                          outBuffSize );
+		}
+		else if( _outputType == "pose_stamped" )
+		{
+			_posePub = nh.advertise<geometry_msgs::PoseStamped>( "resolved_pose", 
+				                                                 outBuffSize );
+		}
+		else if( _outputType == "pose_stamped_with_cov" )
+		{
+			_posePub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>( "resolved_pose", 
+				                                                               outBuffSize );
+		}
+		else
+		{
+			throw std::invalid_argument( "Unknown output type: " + _outputType );
+		}
 	}
 
 private:
 	
-	ros::NodeHandle nodeHandle;
-	ros::NodeHandle privHandle;
+	LookupInterface _lookupInterface;
+	ExtrinsicsInfoManager _extrinsicsManager;
 	
-	LookupInterface lookupInterface;
-	ExtrinsicsInfoManager extrinsicsManager;
-	
-	ros::Publisher posePub;
-	ros::Subscriber poseSub;
-	std::string referenceName;
-	
-	bool CheckName( const std::string& name )
-	{
-		if( extrinsicsManager.HasMember( name ) ) { return true; }
-		return extrinsicsManager.ReadMemberInfo( name, false );
-	}
-	
-	void ProcessInversionAndSend( RelativePose& msg )
-	{
-		RelativePose out = msg;
-		
-		if( out.target_name == referenceName )
-		{
-			std::string temp = out.observer_name;
-			out.observer_name = referenceName;
-			out.target_name = temp;
-			
-			out.relative_pose = PoseToMsg( MsgToPose( out.relative_pose ).Inverse() );
-		}
-		
-		posePub.publish( out );
-	}
+	std::string _outputType;
+	bool _resolveObserver;
+	bool _resolveTarget;
+
+	ros::Publisher _posePub;
+	ros::Subscriber _poseSub; std::string _referenceName;
 	
 	void PoseCallback( const RelativePose::ConstPtr& msg )
 	{
-		RelativePose msgCopy( *msg );
-		std::string observerName = msgCopy.observer_name;
-		std::string targetName = msgCopy.target_name;
-		
-		// If the message already contains the reference, process it and send it out
-		if( observerName == referenceName || targetName == referenceName )
+		RelativePose out( *msg );
+		if( !ProcessRelativePose( out ) ) { return; }
+
+		if( _outputType == "pose" )
 		{
-			ProcessInversionAndSend( msgCopy );
-			return;
+			geometry_msgs::Pose po;
+			po = out.relative_pose;
+			_posePub.publish( po );
 		}
-		
-		PoseSE3 relative = MsgToPose( msg->relative_pose );
-		PoseSE3 transformed;
-		if( CheckName( observerName ) )
+		else if( _outputType == "pose_stamped" )
 		{
-			std::string observerReference = extrinsicsManager.GetInfo( observerName ).referenceFrame;
-			if( observerReference == referenceName )
+			geometry_msgs::PoseStamped po;
+			po.pose = out.relative_pose;
+			po.header.stamp = out.observer_time;
+			po.header.frame_id = out.observer_name;	
+			_posePub.publish( po );
+		}
+		else if( _outputType == "pose_stamped_with_cov" )
+		{
+			geometry_msgs::PoseWithCovarianceStamped po;
+			po.pose.pose = out.relative_pose;
+			po.header.stamp = out.observer_time;
+			po.header.frame_id = out.observer_name;
+			po.pose.covariance.fill( 0 );
+		}
+	}
+
+	void PoseWithCovCallback( const RelativePoseWithCovariance::ConstPtr& msg )
+	{
+		RelativePoseWithCovariance out( *msg );
+		if( !ProcessRelativePose( out.relative_pose ) ) { return; }
+
+		if( _outputType == "pose" )
+		{
+			geometry_msgs::Pose po;
+			po = out.relative_pose.relative_pose;
+			_posePub.publish( po );
+		}
+		else if( _outputType == "pose_stamped" )
+		{
+			geometry_msgs::PoseStamped po;
+			po.pose = out.relative_pose.relative_pose;
+			po.header.stamp = out.relative_pose.observer_time;
+			po.header.frame_id = out.relative_pose.observer_name;	
+			_posePub.publish( po );
+		}
+		else if( _outputType == "pose_stamped_with_cov" )
+		{
+			geometry_msgs::PoseWithCovarianceStamped po;
+			po.pose.pose = out.relative_pose.relative_pose;
+			po.header.stamp = out.relative_pose.observer_time;
+			po.header.frame_id = out.relative_pose.observer_name;
+			po.pose.covariance = out.covariance;
+		}
+	}
+
+	bool ProcessRelativePose( RelativePose& rel )
+	{
+		PoseSE3 pose = MsgToPose( rel.relative_pose );
+		if( _resolveObserver )
+		{
+			if( !_extrinsicsManager.CheckMemberInfo( rel.observer_name ) )
 			{
-				const PoseSE3& observerExtrinsics = extrinsicsManager.GetInfo( observerName ).extrinsics;
-				transformed = observerExtrinsics * relative;
-				msgCopy.observer_name = observerReference;
+				ROS_ERROR_STREAM( "Could not get extrinsics for observer: " << rel.observer_name );
+				return false;
 			}
+			const ExtrinsicsInfo& info = _extrinsicsManager.GetInfo( rel.observer_name );
+			pose = info.extrinsics * pose;
 		}
-		
-		if( CheckName( targetName ) )
+		if( _resolveTarget )
 		{
-			std::string targetReference = extrinsicsManager.GetInfo( targetName ).referenceFrame;
-			if( targetReference == referenceName )
+			if( !_extrinsicsManager.CheckMemberInfo( rel.target_name ) )
 			{
-				const PoseSE3& targetExtrinsics = extrinsicsManager.GetInfo( targetName ).extrinsics;
-				transformed = relative * targetExtrinsics.Inverse();
-				msgCopy.target_name = targetReference;
+				ROS_ERROR_STREAM( "Could not get extrinsics for observer: " << rel.target_name );
+				return false;
 			}
+			const ExtrinsicsInfo& info = _extrinsicsManager.GetInfo( rel.target_name );
+			pose = pose * info.extrinsics.Inverse();
 		}
-		
-		msgCopy.relative_pose = PoseToMsg( transformed );
-		ProcessInversionAndSend( msgCopy );
+		rel.relative_pose = PoseToMsg( pose );
+		return true;
 	}
 };
 
