0109398aa117f63e4fde74fb4cc357299c6b67e9
Humphrey Hu <humhu@cmu.edu>
2016-11-09 09:37:54 -0500

    Simplified laser odometry parameters.

--- odoscan/src/ApproximateVoxelGridFilter.cpp
+++ odoscan/src/ApproximateVoxelGridFilter.cpp
@@ -8,20 +8,15 @@ ApproximateVoxelGridFilter::ApproximateVoxelGridFilter() {}
 
 void ApproximateVoxelGridFilter::InitializeDerived( ros::NodeHandle& ph )
 {
-	FullNumericRange leafSize;
+	double leafSize;
 	GetParamRequired( ph, "voxel_size", leafSize );
-	_leafSize.Initialize( ph, leafSize.init, "voxel_size", 
+	_leafSize.Initialize( ph, leafSize, "voxel_size", 
 	                      "Voxel grid size" );
-	_leafSize.AddCheck<GreaterThanOrEqual>( leafSize.min );
-	_leafSize.AddCheck<LessThanOrEqual>( leafSize.max );
 
-	// FullNumericRange minPointsPerVox;
+	// unsigned int minPointsPerVox;
 	// GetParamRequired( ph, "min_voxel_points", minPointsPerVox );
-	// _minPointsPerVox.Initialize( ph, minPointsPerVox.init, "min_voxel_points",
+	// _minPointsPerVox.Initialize( ph, minPointsPerVox, "min_voxel_points",
 	//                              "Minimum number of points per voxel." );
-	// _minPointsPerVox.AddCheck<GreaterThanOrEqual>( minPointsPerVox.min );
-	// _minPointsPerVox.AddCheck<LessThanOrEqual>( minPointsPerVox.max );
-	// _minPointsPerVox.AddCheck<IntegerValued>( ROUND_CLOSEST );
 }
 
 ScanFilter::FilterType::Ptr ApproximateVoxelGridFilter::CreateFilter()
