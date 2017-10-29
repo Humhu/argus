9f556ec0cb4c81caf168f9310a359ca0244f2bc9
Humphrey Hu <humhu@cmu.edu>
2017-10-29 18:37:08 -0400

    Fixed camera driver default encoding to YUYV

--- camplex/src/DriverNode.cpp
+++ camplex/src/DriverNode.cpp
@@ -58,7 +58,7 @@ DriverNode::DriverNode( ros::NodeHandle& nh, ros::NodeHandle& ph )
 	unsigned int frameRate;
 	GetParam<unsigned int>( ph, "frame_rate", frameRate, 30 );
 	std::string fourCC;
-	GetParam<std::string>( ph, "four_cc", fourCC, "BGR3" );
+	GetParam<std::string>( ph, "four_cc", fourCC, "YUYV" );
 
 	OutputSpecification spec;
 	spec.frameSize.first = frameWidth;
